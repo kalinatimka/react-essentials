b22d993a369a8059ca64ab1a11b68189e6eec9b7
Tsimafei Kalinouski <tsimafei.kalinouski@thomsonreuters.com>
2021-04-30 16:20:54 +0300

    feat: displaying data and images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import restaurant from './restaurant.jpg';
+
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+    </header>
+  );
+}
+
+function Main(props) {
+  return (
+    <section>
+      <p>We serve the most delicious food around.</p>
+      <img src={restaurant} alt='Restaurant'/>
+      <ul>
+        {props.dishes.map(dish => <li key={dish.id}>{dish.title}</li>)}
+      </ul>
+    </section>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>It's true.</p>
+    </footer>
+  );
+}
+
+const dishes = [
+  'Macaroni and Cheese',
+  'Salmon',
+  'Tofu with vegetables',
+  'Minestrone',
+];
+
+const dishesObj = dishes.map((dish, index) => {
+  return {
+    id: index,
+    title: dish,
+  }
+})
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Tsimafei"/>
+      <Main dishes={dishesObj}/>
+      <Footer/>
     </div>
   );
 }
