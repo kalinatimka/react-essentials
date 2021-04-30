846cf280248ee583b39ee4e2ce6d1e94ff4f49e1
Tsimafei Kalinouski <tsimafei.kalinouski@thomsonreuters.com>
2021-04-30 17:36:04 +0300

    feat: add useReducer to the app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [emotion, setEmotion] = useState('happy');
-
-  useEffect(() => {
-    console.log(`It's ${emotion} around here!`);
-  }, [emotion]);
+  const [checked, toggle] = useReducer(
+    (checked) => !checked,
+    false
+  );
 
   return (
     <>
-      <h1>Current emotion is {emotion}</h1>
-      <button onClick={() => setEmotion('happy')}>Happy</button>
-      <button onClick={() => setEmotion('angry')}>Angry</button>
-      <button onClick={() => setEmotion('surprise')}>Surprise</button>
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={toggle}
+      />
+      <p>{checked ? 'checked' : 'unchecked'}</p>
     </>
-  )
+  );
 }
 
 export default App;
