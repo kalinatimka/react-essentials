760ec515fd4b95fc8ff6c166ce3dfb75a4f86d41
Tsimafei Kalinouski <tsimafei.kalinouski@thomsonreuters.com>
2021-05-24 15:00:37 +0300

    start using React testing library

--- src/App.js
+++ src/App.js
@@ -1,44 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
 
-function App({nickname}) {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(null);
-
-  useEffect(() => {
-    if (!nickname) {
-      return;
-    }
-    setLoading(true);
-    fetch(`https://api.github.com/users/${nickname}`)
-      .then(res => res.json())
-      .then(setData)
-      .then(() => {
-        setLoading(false);
-      })
-      .catch(setError);
-  }, [nickname]);
-
-  if (loading) {
-    return <div>Loading...</div>
-  }
-
-  if (error) {
-    return <pre>{JSON.stringify(error, null, 2)}</pre>
-  }
-
-  if (data) {
-    return (
-      <>
-        <h1>{data.name}</h1>
-        <p>{data.location}</p>
-        <img alt={data.login} src={data.avatar_url}/>
-      </>
-    )
-  }
-
-  return <div>No user available</div>
+function App() {
+  return (
+    <div>
+      <h1>Hello React Testing Library</h1>
+    </div>
+  );
 }
 
 export default App;
